f8fecc2c34cdf354ea989e82d3662b3e4feab604
Spay_Shawl <spayshawl@gmail.com>
2022-08-28 10:41:31 +0200

    fix: return on dash

--- src/components/Markdown/markdown.tsx
+++ src/components/Markdown/markdown.tsx
@@ -5,45 +5,44 @@ import stories from '../../stories/stories.json';
 import { Stories } from '../../types/Stories';
 
 type Props = {
-  file_name: string
-}
+	file_name: string;
+};
 
-const Markdown: React.FunctionComponent<Props> = ({
-  file_name
-}) => {
-  const [post, setPost] = useState('');
-  
-  useEffect(() => {
-    import(`../../stories/${file_name}.md`)
-        .then(res => {
-            fetch(res.default)
-                .then(res => res.text())
-                .then(res => setPost(res))
-                .catch(err => console.log(err));
-        })
-        .catch(err => console.log(err));
-  });
+const Markdown: React.FunctionComponent<Props> = ({ file_name }) => {
+	const [post, setPost] = useState('');
 
-  const formatText = (text: string): string => {
-    return text
-      .replaceAll(/(.)(\n){1}\\-/g, `$1\n\n\\-`)
-      .replaceAll('	', '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;')
+	useEffect(() => {
+		import(`../../stories/${file_name}.md`)
+			.then((res) => {
+				fetch(res.default)
+					.then((res) => res.text())
+					.then((res) => setPost(res))
+					.catch((err) => console.log(err));
+			})
+			.catch((err) => console.log(err));
+	});
 
-  }
+	const formatText = (text: string): string => {
+		return text
+			.replaceAll(/(.)\r\n\\-/g, `$1\n\n\\-`)
+			.replaceAll('	', '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;');
+	};
 
-  if(!post) {
-    return null;
-  }
+	if (!post) {
+		return null;
+	}
 
-  return (
-    <div className="markdown">
-      <a className="back" href='/'><BsArrowLeftCircle></BsArrowLeftCircle></a>
-      <mk.default className="markdown-body">
-        {formatText(post)}
-      </mk.default>
-      <p className="author">Écrit par {(stories as Stories)[file_name].authors.join(' & ')}</p>
-    </div>
-  );
-}
+	return (
+		<div className="markdown">
+			<a className="back" href="/">
+				<BsArrowLeftCircle></BsArrowLeftCircle>
+			</a>
+			<mk.default className="markdown-body">{formatText(post)}</mk.default>
+			<p className="author">
+				Écrit par {(stories as Stories)[file_name].authors.join(' & ')}
+			</p>
+		</div>
+	);
+};
 
 export default Markdown;
